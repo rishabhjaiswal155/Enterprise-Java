66807aae8c20b4eb3e3a9ae7d89ca29cd145df3c
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-09-30 16:31:14 +0530

    Created the empty Spring bean configuration xml file,Added the dependency and dependent objects and test it using tester using Setter based Dependency Injection by Programmer.

---
+++ Spring_MavenBased_Standalone_project/src/main/java/tester/TestATM.java
@@ -0,0 +1,16 @@
+package tester;
+
+import dependency.SoapTransport;
+import dependent.ATMImpl;
+
+public class TestATM {
+
+	public static void main(String[] args) {
+		//dependent obj
+		ATMImpl ref1=new ATMImpl();
+		ref1.setMyTransport(new SoapTransport());
+		ref1.deposit(1000);
+
+	}
+
+}
