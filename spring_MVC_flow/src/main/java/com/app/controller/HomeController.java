5cbe4c6e83031dbb74922d59f5b59ade2fd607df
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-10-02 15:43:30 +0530

    Spring MVC flow with Dispatcher Servlet,spring-servlet.xml, @Controller,@RequestMapping() for request handling method.

---
+++ spring_MVC_flow/src/main/java/com/app/controller/HomeController.java
@@ -0,0 +1,31 @@
+package com.app.controller;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+//singleton and eager
+@Controller()
+public class HomeController {
+	public HomeController() {
+	System.out.println("in ctor of "+getClass());	
+	}
+	@PostConstruct
+	public void anyInit() {
+		System.out.println("in init of "+getClass());
+	}
+	@PreDestroy
+	public void anyDestroy() {
+		System.out.println("in destroy of "+getClass());
+	}
+	
+	//request handling method
+	@RequestMapping("/hello")
+	public String sayHello1() {
+		System.out.println("in sayHello1 method of "+getClass());
+		return "/welcome";//handler returns Logical view name to d.s
+		//D.s forward it to  view resolver and view resolver convert logical view name to actual view name and forward it to d.s
+	}
+
+}
