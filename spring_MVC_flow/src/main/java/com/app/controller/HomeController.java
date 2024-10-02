6b158e9b66695944eaf7eb53c373e9de99beee6d
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-10-02 19:28:39 +0530

    Moving index.jsp from webApp to WebInF/views for functional separation i.e Now index.jsp is also handled by Spring Container by requestHandling method @GetMapping("/") in HomeController.

--- spring_MVC_flow/src/main/java/com/app/controller/HomeController.java
+++ spring_MVC_flow/src/main/java/com/app/controller/HomeController.java
@@ -4,9 +4,10 @@ import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 //singleton and eager
-@Controller()
+@Controller
 public class HomeController {
 	public HomeController() {
 	System.out.println("in ctor of "+getClass());	
@@ -26,6 +27,13 @@ public class HomeController {
 		System.out.println("in sayHello1 method of "+getClass());
 		return "/welcome";//handler returns Logical view name to d.s
 		//D.s forward it to  view resolver and view resolver convert logical view name to actual view name and forward it to d.s
+	}
+	
+	//request handling method for index
+	@GetMapping("/")
+	public String showIndexPage() {
+		System.out.println("in showIndexPage() of "+getClass());
+		return "/index";
 	}
 
 }
