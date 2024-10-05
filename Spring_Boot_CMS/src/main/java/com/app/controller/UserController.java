73afdee762a030a898e40214ae354ef8357eb9a4
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-10-05 16:24:16 +0530

    solved the double submit issue by using keyword redirect:,also used the addFlashAttribute() method of RedirectAttribute interface,used the session.setAttribute() of HttpSession i/f for session Management and Model i/f's addAttribute() to exchange the data from controller to view layer.

--- Spring_Boot_CMS/src/main/java/com/app/controller/UserController.java
+++ Spring_Boot_CMS/src/main/java/com/app/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.app.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.app.pojos.Role;
 import com.app.pojos.User;
@@ -28,12 +31,12 @@ public String showLoginForm() {
 }
 
 @PostMapping("/login")
-public String processLoginForm(@RequestParam String email,@RequestParam String password,Model map) {
+public String processLoginForm(@RequestParam String email,@RequestParam String password,Model map,HttpSession session,RedirectAttributes flashmap) {
 	System.out.println("in processLoginForm() of "+getClass()+" "+email+" "+password+" "+map);
 	try {
 	User authenticateUser=userService.authenticateUser(email, password);
-	map.addAttribute("msg", "Login Sucessfull");
-	map.addAttribute("user_dtls",authenticateUser);
+	flashmap.addFlashAttribute("msg", "Login Sucessfull");
+	session.setAttribute("user_dtls",authenticateUser);
 	if(authenticateUser.getRoles().contains(new Role(UserRole.ADMIN))) {
 		return "redirect:/admin/status";
 	}
