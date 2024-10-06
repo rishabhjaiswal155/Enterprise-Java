e7d1f414b833554d4bc5cc294a363e31f5cb5bca
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-10-06 17:00:14 +0530

    Created the EmployeeController and added the end point to handle the end to end request.

---
+++ REST_API_Employee_CRUD/src/main/java/com/app/controller/EmployeeController.java
@@ -0,0 +1,31 @@
+package com.app.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.app.entities.Employee;
+import com.app.service.IEmployeeService;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+	
+	@Autowired
+	private IEmployeeService empService;
+	
+	public EmployeeController() {
+	System.out.println("in ctor of "+getClass());
+	}
+	
+	//provide end point
+	@GetMapping
+	public List<Employee> getAllEmployeeDetails(){
+		System.out.println("in getAllEmployeesDetails() of "+getClass());
+		return empService.getAllEmployeeDetails();
+	}
+
+}
