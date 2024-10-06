670542e9a7b222de9d5ee9670708f98bf9efc8c1
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-10-06 18:24:33 +0530

    Added the React-FrontEnd App for Employee

--- REST_API_Employee_CRUD/src/main/java/com/app/controller/EmployeeController.java
+++ REST_API_Employee_CRUD/src/main/java/com/app/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.app.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.app.service.IEmployeeService;
 
 @RestController
 @RequestMapping("/api/employees")
+@CrossOrigin(origins = "http://localhost:3000")
 public class EmployeeController {
 	
 	@Autowired
