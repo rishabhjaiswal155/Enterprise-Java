5c01321a552c7af81ad29d0ed411e6cce701118b
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-10-30 17:21:27 +0530

    Created the separate Dto for Employee which includes the all validation annotation and Json related annotation like @JsonProperty(access=Access.WRITEONLY).

---
+++ REST_API_Employee_CRUD/src/main/java/com/app/dto/EmployeeDto.java
@@ -0,0 +1,46 @@
+package com.app.dto;
+
+import java.time.LocalDate;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Future;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Range;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class EmployeeDto{
+	@NotBlank(message = "firstName is required")
+	private String name;
+	@NotBlank(message="lastName is required")
+	//@Min(value=4,message="lastName minimun chars must be 4")
+	//@Max(value=20,message="lastName mximum chars must be 20")
+	private String lastName;
+	@Email(message="Email must be in required format")
+	private String email;
+	@Pattern(regexp="((?=.*\\d)(?=.*[a-z])(?=.*[#@$*]).{5,20})")
+	@JsonProperty(access=Access.WRITE_ONLY)
+	private String password;
+	@NotBlank(message="location is required")
+	private String location;
+	@JsonProperty(value = "department")
+	private String dept;
+	@NotNull
+	@Range(min=10000,max=90000)
+	private double salary;
+	@Future(message = "joinDate must be a future date")
+	private LocalDate joinDate;
+}
--- REST_API_Employee_CRUD/src/main/java/com/app/entities/Employee.java
+++ REST_API_Employee_CRUD/src/main/java/com/app/entities/Employee.java
@@ -5,14 +5,6 @@ import java.time.LocalDate;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Future;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-
-import org.hibernate.validator.constraints.Range;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -29,26 +21,17 @@ import lombok.ToString;
 @ToString
 public class Employee extends BaseEntity {
 	@Column(length=20)
-	@NotBlank(message = "firstName is required")
 	private String name;
 	@Column(length=20,unique=true)
-	@NotBlank(message="lastName is required")
-	//@Min(value=4,message="lastName minimun chars must be 4")
-	//@Max(value=20,message="lastName mximum chars must be 20")
 	private String lastName;
-	@Email(message="Email must be in required format")
 	private String email;
-	@Pattern(regexp="((?=.*\\d)(?=.*[a-z])(?=.*[#@$*]).{5,20})")
 	private String password;
 	@Column(length=20)
-	@NotBlank(message="location is required")
 	private String location;
-	@Column(length=20,name="department_name")
+
 	@JsonProperty(value = "department")
+	@Column(length=20,name="department_name")
 	private String dept;
-	@NotNull
-	@Range(min=10000,max=90000)
-	private double salary;
-	@Future(message = "joinDate must be a future date")
+    private double salary;
 	private LocalDate joinDate;
 }
