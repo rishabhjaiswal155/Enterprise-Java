cc30be49d31f75b8cac1488c853109f057c9c4a7
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-10-30 17:13:18 +0530

    Added the ModelMapper dependency in pom.xml and configure it in Application.java file using @Bean and creating the instance of ModelMapper with strict strategy.

--- REST_API_Employee_CRUD/src/main/java/com/app/RestApiEmployeeCrudApplication.java
+++ REST_API_Employee_CRUD/src/main/java/com/app/RestApiEmployeeCrudApplication.java
@@ -1,13 +1,21 @@
 package com.app;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class RestApiEmployeeCrudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiEmployeeCrudApplication.class, args);
 	}
-
+@Bean
+public ModelMapper modelMapper() {
+	System.out.println("in modelmapper");
+	ModelMapper mapper=new ModelMapper();
+	mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+	return mapper;
+}
 }
