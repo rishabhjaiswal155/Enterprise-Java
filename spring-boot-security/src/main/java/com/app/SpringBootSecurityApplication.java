37a2acd846ba198a762efb7fb3e267fb8d32a2bf
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-10-31 18:47:55 +0530

    Added the PasswordEncoder BCryptPasswordEncoder in Application.java and added the dependency in SecurityConfig.java class to allow the encode() of passwodEncoder to be use in password(enc.encode("password")).

--- spring-boot-security/src/main/java/com/app/SpringBootSecurityApplication.java
+++ spring-boot-security/src/main/java/com/app/SpringBootSecurityApplication.java
@@ -2,12 +2,20 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class SpringBootSecurityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSecurityApplication.class, args);
+	}
+	
+	@Bean
+	public PasswordEncoder encoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
--- spring-boot-security/src/main/java/com/app/config/SecurityConfig.java
+++ spring-boot-security/src/main/java/com/app/config/SecurityConfig.java
@@ -1,20 +1,24 @@
 package com.app.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private PasswordEncoder enc;
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
-		.withUser("Rishi").password("Rishi@123").roles("admin")
+		.withUser("Rishi").password(enc.encode("Rishi@123")).roles("user")
 		.and()
-		.withUser("Raj").password("Raj@123").roles("admin","user");
+		.withUser("Raj").password(enc.encode("Raj@123")).roles("admin","user");
 	}
 
 	@Override
